d4dcf548fb443b697915c5e55a7ed92c1aeba61b
Latish Sehgal <latish.sehgal@gmail.com>
2015-03-30 17:27:02 -0500

    Week 2 Assgn

--- Android/HelloDojo/UserLogin.cs
+++ Android/HelloDojo/UserLogin.cs
@@ -1,32 +1,48 @@
 ﻿using System;
+using Assisticant.Fields;
 
 namespace HelloDojo
 {
-    public class UserLogin
-    {
-        private string _userName;
-        private string _password;
-        private string _message;
 
-        public string UserName {
-            get { return _userName; }
-            set { _userName = value; }
-        }
+	public class UserLoginViewModel
+	{
+		public UserLoginViewModel()
+		{
+			UserLogin = new UserLogin ();
+		}
+		private Observable<string> _message = new Observable<string> ();
 
-        public string Password {
-            get { return _password; }
-            set { _password = value; }
-        }
+		public UserLogin UserLogin {
+			get;
+			set;
+		}
 
-        public void Login() {
-            _message = string.Format (
-                "Welcome, {0}, your password is {1}",
-                _userName, _password);
-        }
+		public string Message {
+			get { return _message; }
+		}
 
-        public string Message {
-            get { return _message; }
-        }
-    }
+		public void Login ()
+		{
+			_message.Value = string.Format (
+				"Welcome, {0}, your password is {1}",
+				UserLogin.UserName, UserLogin.Password);
+		}
+	}
+
+	public class UserLogin
+	{
+		private Observable<string> _userName = new Observable<string> ();
+		private Observable<string> _password = new Observable<string> ();
+
+		public string UserName {
+			get { return _userName; }
+			set { _userName.Value = value; }
+		}
+
+		public string Password {
+			get { return _password; }
+			set { _password.Value = value; }
+		}
+	}
 }
 
